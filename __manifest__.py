0943ed0cc3b1368ec28288eabf255a8f3871d415
xtfngg <376383373@qq.com>
2017-11-10 16:00:18 +0800

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Devices",
+
+    'summary': "Computer equipment management",
+
+    'description': "Computer equipment status, use the status",
+
+    'author': "xtfngg",
+    'website': "http://www.xtfngg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mail'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
